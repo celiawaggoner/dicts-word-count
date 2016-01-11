5e95054691972275605bd6442c040ff327343194
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 12:55:58 -0800

    updated after code review.

--- wordcount.py
+++ wordcount.py
@@ -6,32 +6,27 @@ def word_count(filename):
     #open the file
     the_file = open(filename)
 
-    #split words by space
-
-    all_words = []
-
-    for line in the_file:
-        line = line.rstrip()
-        words = line.split(" ")
-        all_words += words
-    # print all_words
-
     #create an empty dictionary 
     word_count_dict = {}
 
-    #loop over all_words
-    #if work exists in dictionary, value + 1
-    #else create new key for word and assign value to 1
+    #split words by space
+    #loop over the_file for word
+    #if word exists in dictionary, count + 1
+    #else create new key for word and assign count to 1
 
-    for word in all_words:
-        if word in word_count_dict:
-            word_count_dict[word] += 1
-        else:
-            word_count_dict[word] = 1
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split()
+        for word in words:
+            if word in word_count_dict:
+                word_count_dict[word] += 1
+            else:
+                word_count_dict[word] = 1
 
-    #print word and word count
     
+    #loop over dictionary, print word and word count
     for word, count in word_count_dict.iteritems():
+    #for word, count in word_count_dict.items():
         print "%s %d" % (word, count)
 
 word_count("twain.txt")
