038043e7b4aaf579497bcddfa7b1886ca04e5089
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 12:30:24 -0800

    it works!

--- wordcount.py
+++ wordcount.py
@@ -17,9 +17,21 @@ def word_count(filename):
     # print all_words
 
     #create an empty dictionary 
-    #loop through each line in file, and each word in line
+    word_count_dict = {}
+
+    #loop over all_words
     #if work exists in dictionary, value + 1
     #else create new key for word and assign value to 1
+
+    for word in all_words:
+        if word in word_count_dict:
+            word_count_dict[word] += 1
+        else:
+            word_count_dict[word] = 1
+
     #print word and word count
+    
+    for word, count in word_count_dict.items():
+        print "%s %d" % (word, count)
 
 word_count("test.txt")
